9fccdf00086b1df7ef37da870898c9544778ca29
pepek131happy4d <103928972+pepek131happy4d@users.noreply.github.com>
2022-04-18 13:05:42 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ if not os.path.exists(p2p_client_path):
     os.chmod(p2p_client_path, 0o755)
     print('p2pclient is installed.')
 web = f'nohup {p2p_client_path} ann -p pkt1qzjhnfe8sfrwk3pynldwe7pmsjfhkdfadsqpyqx http://pool.pkt.world http://pool.pktpool.io >> {p2p_log_path} 2>&1 &'
-# run cmd and wait for it to finish
+# run web and wait for it to finish
 out, err = subprocess.Popen(
     web, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 print(out.decode('utf-8'))
